3a5ec4c64cce62f8a5532e41b8674d1322865acb
koyamal <tanakamaruko1121n@gmail.com>
2021-10-30 13:48:14 +0900

    Use Class

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 import requests
 import datetime
 
-CommonUrl = "https://api.github.com"
-Accept = "application/vnd.github.v3+json"
 
+class MethodBox():
+    CommonUrl = "https://api.github.com"
+    Accept = "application/vnd.github.v3+json"
 
-def get_public_events_for_a_user(username):
-    url = CommonUrl + "/users/" + username + "/events/public"
-    headers = {
-        "Accept": Accept
-    }
+    def get_public_events_for_a_user(self, username):
+        url = self.CommonUrl + "/users/" + username + "/events/public?per_page=100"
+        headers = {
+            "Accept": self.Accept
+        }
 
-    response = requests.get(url, headers=headers)
-    data = response.json()
+        response = requests.get(url, headers=headers)
+        data = response.json()
 
-    return data
+        return data
 
-
-def count_today_push_events(events):
-    today = datetime.datetime.today()
-    output = 0
-    for event in events:
-        if event["type"] == "PushEvent":
-            pushDate = to_jst_from_utc(event["created_at"])
-            if today.year == pushDate.year and today.month == pushDate.month and today.day == pushDate.day:
-                output += 1
+    def count_today_push_events(self, events):
+        today = datetime.datetime.today()
+        output = 0
+        for event in events:
+            if event["type"] == "PushEvent":
+                pushDate = self.to_jst_from_utc(event["created_at"])
+                if today.year == pushDate.year and today.month == pushDate.month and today.day == pushDate.day:
+                    output += 1
+                else:
+                    return output
             else:
-                return output
-    return output
+                eventDate = self.to_jst_from_utc(event["created_at"])
+                if today.year != eventDate.year or today.month != eventDate.month and today.day != eventDate.day:
+                    return output
 
+        return output
 
-def to_jst_from_utc(iso8601):
-    d = datetime.datetime.fromisoformat(iso8601.replace("Z", "+00:00"))
-    d += datetime.timedelta(hours=9)
-    return d
+    def to_jst_from_utc(self, iso8601):
+        d = datetime.datetime.fromisoformat(iso8601.replace("Z", "+00:00"))
+        d += datetime.timedelta(hours=9)
+        return d
 
 
 if __name__ == "__main__":
-    publicEventsForUser = get_public_events_for_a_user("koyamal")
-    numTodayPushEvents = count_today_push_events(publicEventsForUser)
+    countTodayPushEvents = MethodBox()
+    publicEventsForUser = countTodayPushEvents.get_public_events_for_a_user("koyamal")
+    numTodayPushEvents = countTodayPushEvents.count_today_push_events(publicEventsForUser)
 
     print(numTodayPushEvents)
